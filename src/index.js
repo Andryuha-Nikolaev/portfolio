72d160ec7d1893693ea07b8840d18358b7fde2e1
Andrey Nikolaev <andryuha_nikolaev@mail.ru>
2023-02-21 18:01:29 +0300

    feat: параллакс в хедере и эффект свечения текста

--- src/index.js
+++ src/index.js
@@ -2,174 +2,183 @@ import './index.html';
 import './scss/index.scss';
 import { mult } from './modules/main';
 
-console.log(mult(3, 4));
-
-const items = [
-  {
-    image: 'https://mywebicons.ru/i/jpg/28c31e555b3642d94da68bbbb22ed497.jpg',
-    title: 'Вопрос 1',
-    description:
-      'Это популярная серия программируемых робототехнических конструкторов компании Robotis. Серия представлена разнообразными универсальными наборами, которые подойдут как начинающим робототехникам, так и специалистам, работающим над решением актуальных робототехнических задач. Также в наборе есть пульт для управления роботом и набор пластиковых элементов для придания уникального вида собранным моделям.',
-    price: '82 000',
-  },
-  {
-    image: 'https://code.s3.yandex.net/web-code/oop/card_detail.jpg',
-    title: 'Вопрос 2',
-    description:
-      'BIOLOID Premium kit – набор для создания различных шагающих роботов на основе моторов Dynamixel и контроллера СМ-530, для образования, игр и соревнований.',
-    price: '126 000',
-  },
-  {
-    image: 'https://code.s3.yandex.net/web-code/oop/card_detail2.png',
-    title: 'Вопрос 3',
-    description:
-      'Модель позволяет использовать при движении и педали и электрическую тягу, а также их сочетание. Съемный аккумулятор легко заменяется во время поездки, позволяя тем самым существенно увеличивать ее продолжительность.',
-    price: '145 000',
-  },
-  {
-    image: 'https://mywebicons.ru/i/jpg/28c31e555b3642d94da68bbbb22ed497.jpg',
-    title: 'Вопрос 4',
-    description:
-      'Это популярная серия программируемых робототехнических конструкторов компании Robotis. Серия представлена разнообразными универсальными наборами, которые подойдут как начинающим робототехникам, так и специалистам, работающим над решением актуальных робототехнических задач. Также в наборе есть пульт для управления роботом и набор пластиковых элементов для придания уникального вида собранным моделям.',
-    price: '82 000',
-  },
-  {
-    image: 'https://code.s3.yandex.net/web-code/oop/card_detail.jpg',
-    title: 'Вопрос 5',
-    description:
-      'BIOLOID Premium kit – набор для создания различных шагающих роботов на основе моторов Dynamixel и контроллера СМ-530, для образования, игр и соревнований.',
-    price: '126 000',
-  },
-  {
-    image: 'https://code.s3.yandex.net/web-code/oop/card_detail2.png',
-    title: 'Вопрос 6',
-    description:
-      'Модель позволяет использовать при движении и педали и электрическую тягу, а также их сочетание. Съемный аккумулятор легко заменяется во время поездки, позволяя тем самым существенно увеличивать ее продолжительность.',
-    price: '145 000',
-  },
-];
-
-const cardList = document.querySelector('.elements__list');
-const popupElement = document.querySelector('.popup');
-const popupImage = document.querySelector('.popup__image');
-const popupCloseButton = document.querySelector('.popup__close');
-const defaultCardButton = document.querySelector('.filter__button_type_grid');
-const altCardButton = document.querySelector('.filter__button_type_column');
-
-defaultCardButton.addEventListener('click', () => {
-  renderElements(false);
-  defaultCardButton.classList.add('active');
+// parallax
+document.addEventListener('scroll', function () {
+  // document.body.style.cssText = `--scrollTop: ${this.scrollY}px`;
+  console.log(window.scrollY);
+  document.body.style.setProperty('--scrollTop', `${window.scrollY}px`);
 });
 
-altCardButton.addEventListener('click', () => {
-  renderElements(true);
-});
+// window.addEventListener('scroll', function () {
+//   console.log('suc');
+// });
 
-class Card {
-  constructor(templateSelector) {
-    this._templateSelector = templateSelector;
-  }
+// const items = [
+//   {
+//     image: 'https://mywebicons.ru/i/jpg/28c31e555b3642d94da68bbbb22ed497.jpg',
+//     title: 'Вопрос 1',
+//     description:
+//       'Это популярная серия программируемых робототехнических конструкторов компании Robotis. Серия представлена разнообразными универсальными наборами, которые подойдут как начинающим робототехникам, так и специалистам, работающим над решением актуальных робототехнических задач. Также в наборе есть пульт для управления роботом и набор пластиковых элементов для придания уникального вида собранным моделям.',
+//     price: '82 000',
+//   },
+//   {
+//     image: 'https://code.s3.yandex.net/web-code/oop/card_detail.jpg',
+//     title: 'Вопрос 2',
+//     description:
+//       'BIOLOID Premium kit – набор для создания различных шагающих роботов на основе моторов Dynamixel и контроллера СМ-530, для образования, игр и соревнований.',
+//     price: '126 000',
+//   },
+//   {
+//     image: 'https://code.s3.yandex.net/web-code/oop/card_detail2.png',
+//     title: 'Вопрос 3',
+//     description:
+//       'Модель позволяет использовать при движении и педали и электрическую тягу, а также их сочетание. Съемный аккумулятор легко заменяется во время поездки, позволяя тем самым существенно увеличивать ее продолжительность.',
+//     price: '145 000',
+//   },
+//   {
+//     image: 'https://mywebicons.ru/i/jpg/28c31e555b3642d94da68bbbb22ed497.jpg',
+//     title: 'Вопрос 4',
+//     description:
+//       'Это популярная серия программируемых робототехнических конструкторов компании Robotis. Серия представлена разнообразными универсальными наборами, которые подойдут как начинающим робототехникам, так и специалистам, работающим над решением актуальных робототехнических задач. Также в наборе есть пульт для управления роботом и набор пластиковых элементов для придания уникального вида собранным моделям.',
+//     price: '82 000',
+//   },
+//   {
+//     image: 'https://code.s3.yandex.net/web-code/oop/card_detail.jpg',
+//     title: 'Вопрос 5',
+//     description:
+//       'BIOLOID Premium kit – набор для создания различных шагающих роботов на основе моторов Dynamixel и контроллера СМ-530, для образования, игр и соревнований.',
+//     price: '126 000',
+//   },
+//   {
+//     image: 'https://code.s3.yandex.net/web-code/oop/card_detail2.png',
+//     title: 'Вопрос 6',
+//     description:
+//       'Модель позволяет использовать при движении и педали и электрическую тягу, а также их сочетание. Съемный аккумулятор легко заменяется во время поездки, позволяя тем самым существенно увеличивать ее продолжительность.',
+//     price: '145 000',
+//   },
+// ];
 
-  //   Чтобы получить нужную разметку, добавьте классу Card метод _getTemplate, который:
-  // найдёт template-элемент с классом horizontal-card,
-  // извлечёт его содержимое,
-  // в содержимом найдёт элемент с классом card,
-  // клонирует его,
-  // вернёт клонированный элемент.
-  _getTemplate() {
-    const cardElement = document
-      .querySelector(this._templateSelector)
-      .content.querySelector('.elements__list-item')
-      .cloneNode(true);
+// const cardList = document.querySelector('.elements__list');
+// const popupElement = document.querySelector('.popup');
+// const popupImage = document.querySelector('.popup__image');
+// const popupCloseButton = document.querySelector('.popup__close');
+// const defaultCardButton = document.querySelector('.filter__button_type_grid');
+// const altCardButton = document.querySelector('.filter__button_type_column');
 
-    // вернём DOM-элемент карточки
-    return cardElement;
-  }
+// defaultCardButton.addEventListener('click', () => {
+//   renderElements(false);
+//   defaultCardButton.classList.add('active');
+// });
 
-  _setEventListeners() {
-    this._element.addEventListener('click', () => {
-      this._handleOpenPopup();
-    });
-    popupCloseButton.addEventListener('click', () => {
-      this._handleClosePopup();
-    });
-  }
+// altCardButton.addEventListener('click', () => {
+//   renderElements(true);
+// });
 
-  _handleClosePopup() {
-    popupImage.src = '';
-    popupElement.classList.remove('popup_is-opened');
-  }
+// class Card {
+//   constructor(templateSelector) {
+//     this._templateSelector = templateSelector;
+//   }
 
-  _handleOpenPopup() {
-    popupImage.src = this._image;
-    popupElement.classList.add('popup_is-opened');
-  }
-}
+//   //   Чтобы получить нужную разметку, добавьте классу Card метод _getTemplate, который:
+//   // найдёт template-элемент с классом horizontal-card,
+//   // извлечёт его содержимое,
+//   // в содержимом найдёт элемент с классом card,
+//   // клонирует его,
+//   // вернёт клонированный элемент.
+//   _getTemplate() {
+//     const cardElement = document
+//       .querySelector(this._templateSelector)
+//       .content.querySelector('.elements__list-item')
+//       .cloneNode(true);
 
-class DefaultCard extends Card {
-  constructor(data, templateSelector) {
-    super(templateSelector);
-    this._title = data.title;
-    this._image = data.image;
-    this._description = data.description;
-  }
-  generateCard() {
-    this._element = super._getTemplate();
-    super._setEventListeners(); //метод создаст карточки уже с обработчиком событий
+//     // вернём DOM-элемент карточки
+//     return cardElement;
+//   }
 
-    this._cardImage = this._element.querySelector('.elements__image'); //изображение карточки
-    this._cardText = this._element.querySelector('.elements__title'); //описание карточки
-    this._cardImage.src = this._image;
-    this._cardImage.alt = this._title;
-    this._cardText.textContent = this._title;
+//   _setEventListeners() {
+//     this._element.addEventListener('click', () => {
+//       this._handleOpenPopup();
+//     });
+//     popupCloseButton.addEventListener('click', () => {
+//       this._handleClosePopup();
+//     });
+//   }
 
-    return this._element;
-  }
-}
+//   _handleClosePopup() {
+//     popupImage.src = '';
+//     popupElement.classList.remove('popup_is-opened');
+//   }
 
-class AltCard extends Card {
-  constructor(data, templateSelector) {
-    super(templateSelector);
-    this._title = data.title;
-    this._image = data.image;
-    this._description = data.description;
-  }
-  generateCard() {
-    this._element = super._getTemplate();
-    super._setEventListeners(); //метод создаст карточки уже с обработчиком событий
+//   _handleOpenPopup() {
+//     popupImage.src = this._image;
+//     popupElement.classList.add('popup_is-opened');
+//   }
+// }
 
-    this._cardImage = this._element.querySelector('.elements__image'); //изображение карточки
-    this._cardText = this._element.querySelector('.elements__title'); //описание карточки
-    this._cardImage.src = this._image;
-    this._cardImage.alt = this._title;
-    this._cardText.textContent = this._title;
+// class DefaultCard extends Card {
+//   constructor(data, templateSelector) {
+//     super(templateSelector);
+//     this._title = data.title;
+//     this._image = data.image;
+//     this._description = data.description;
+//   }
+//   generateCard() {
+//     this._element = super._getTemplate();
+//     super._setEventListeners(); //метод создаст карточки уже с обработчиком событий
 
-    return this._element;
-  }
-}
+//     this._cardImage = this._element.querySelector('.elements__image'); //изображение карточки
+//     this._cardText = this._element.querySelector('.elements__title'); //описание карточки
+//     this._cardImage.src = this._image;
+//     this._cardImage.alt = this._title;
+//     this._cardText.textContent = this._title;
 
-const renderElements = (isAlt) => {
-  cardList.innerHTML = '';
-  if (isAlt) {
-    console.log(isAlt);
-    cardList.classList.add('elements__alt');
-  } else {
-    cardList.classList.remove('elements__alt');
-  }
+//     return this._element;
+//   }
+// }
 
-  let randomItem = items[Math.floor(Math.random() * items.length)];
-  console.log(randomItem);
+// class AltCard extends Card {
+//   constructor(data, templateSelector) {
+//     super(templateSelector);
+//     this._title = data.title;
+//     this._image = data.image;
+//     this._description = data.description;
+//   }
+//   generateCard() {
+//     this._element = super._getTemplate();
+//     super._setEventListeners(); //метод создаст карточки уже с обработчиком событий
 
-  items.forEach((item) => {
-    // если isGrid === true создавайте DefaultCard
-    //  иначе HorizontalCard
-    const card = isAlt
-      ? new AltCard(item, '#template-alternative')
-      : new DefaultCard(item, '#template-default');
+//     this._cardImage = this._element.querySelector('.elements__image'); //изображение карточки
+//     this._cardText = this._element.querySelector('.elements__title'); //описание карточки
+//     this._cardImage.src = this._image;
+//     this._cardImage.alt = this._title;
+//     this._cardText.textContent = this._title;
 
-    const cardElement = card.generateCard();
-    cardList.append(cardElement);
-  });
-};
+//     return this._element;
+//   }
+// }
 
-renderElements();
+// const renderElements = (isAlt) => {
+//   cardList.innerHTML = '';
+//   if (isAlt) {
+//     console.log(isAlt);
+//     cardList.classList.add('elements__alt');
+//   } else {
+//     cardList.classList.remove('elements__alt');
+//   }
+
+//   let randomItem = items[Math.floor(Math.random() * items.length)];
+//   console.log(randomItem);
+
+//   items.forEach((item) => {
+//     // если isGrid === true создавайте DefaultCard
+//     //  иначе HorizontalCard
+//     const card = isAlt
+//       ? new AltCard(item, '#template-alternative')
+//       : new DefaultCard(item, '#template-default');
+
+//     const cardElement = card.generateCard();
+//     cardList.append(cardElement);
+//   });
+// };
+
+// renderElements();
